23eaa88ff33dfdb3acc28cda6e05296b099c7575
Harris-GAV <61297860+Harris-GAV@users.noreply.github.com>
2020-02-21 20:17:05 -0500

    Add files via upload
    unfinished start 
    board and point classes 
    need win condition checks

---
+++ 3701_TicTacToeApp-master/3701_TicTacToeApp-master/TicTacToe/src/tictactoe/Tictactoe (2).java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Tictactoe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public void start(){
+        
+        char [][] gameBoard = {{' ','|',' ','|',' '},
+            {' ','|',' ','|',' '},
+            {' ','|',' ','|',' '},
+            {' ','|',' ','|',' '},
+            {' ','|',' ','|',' '}};
+        
+        for(char [] row: gameBoard){
+            for (char c: row){
+                System.out.println(gameBoard);
+            }
+        }
+                
+        }
+    }
+
